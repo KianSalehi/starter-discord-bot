95a8e5091d81cdf9b32873db3c44a6d294c8ba9c
KianSalehi <kian.k.salehi@gmail.com>
2023-05-10 09:54:35 -0400

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ require('dotenv').config()
 const APPLICATION_ID = process.env.APPLICATION_ID 
 const TOKEN = process.env.TOKEN 
 const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
-const GUILD_ID = process.env.GUILD_ID 
 
 
 const axios = require('axios')
@@ -16,7 +15,7 @@ const app = express();
 // app.use(bodyParser.json());
 
 const discord_api = axios.create({
-  baseURL: 'https://discord.com/api/',
+  baseURL: 'https://discord.com/api/v10',
   timeout: 3000,
   headers: {
 	"Access-Control-Allow-Origin": "*",
@@ -66,39 +65,51 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
         }
       });
     }
+    if (interaction.data.name == 'play'){
+      console.log(interaction.data)
+    }
   }
 
 });
 
 
-
-app.get('/register_commands', async (req,res) =>{
+app.get('/r', async (req, res) =>{
   let slash_commands = [
     {
-      "name": "yo",
-      "description": "replies with Yo!",
+      "name": "play",
+      "description": "Play a song",
+      "options": [{
+        "name": "song",
+        "type": "STRING",
+        "description": "The URL or search term of the song",
+        "required": "true"
+      }]
+    },
+    {
+      "name": "stop",
+      "description": "Stop the music",
       "options": []
     },
     {
-      "name": "dm",
-      "description": "sends user a DM",
+      "name": "queue",
+      "description": "Show the queue of songs",
+      "options": []
+    },
+    {
+      "name": "skip",
+      "description": "Skip to the next song",
       "options": []
     }
   ]
-  try
-  {
-    // api docs - https://discord.com/developers/docs/interactions/application-commands#create-global-application-command
-    let discord_response = await discord_api.put(
-      `/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
-      slash_commands
-    )
-    console.log(discord_response.data)
-    return res.send('commands have been registered')
+  try{
+    discordResponse = discord_api.post(`/applications/${APPLICATION_ID}/commands`, json=slash_commands)
+    return res.send(`Registered Commands`)
   }catch(e){
     console.error(e.code)
     console.error(e.response?.data)
     return res.send(`${e.code} error from discord`)
   }
+    
 })
 
 
